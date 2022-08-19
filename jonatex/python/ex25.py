b6edacbd6850f7382b7cefbe4aa92fa101c5d164
jonatex <108312517+jonatex@users.noreply.github.com>
2022-08-18 22:34:51 -0600

    tarea4

--- jonatex/python/ex25.py
+++ jonatex/python/ex25.py
@@ -4,3 +4,36 @@ texto = "el profe hoy si me explico bien."
 
 palabras = exs25.break_words(texto)
 print(palabras)
+
+texto = "el profe hoy si me explico bien."
+
+palabras = exs25.sort_words(texto)
+print(palabras)
+
+
+texto = "el profe hoy si me explico bien."
+
+
+palabras = exs25.print_first_word(texto)
+print(palabras)
+
+texto = "el profe hoy si me explico bien."
+
+palabras = exs25.print_last_word(texto)
+print(palabras)
+
+texto = "el profe hoy si me explico bien."
+
+palabras = exs25.sort_sentence(texto)
+print(palabras)
+
+
+texto = "el profe hoy si me explico bien."
+
+palabras = exs25.print_first_and_last(texto)
+print(palabras)
+
+texto = "el profe hoy si me explico bien."
+
+palabras = exs25.sortprint_first_and_last_ed(texto)
+print(palabras)
