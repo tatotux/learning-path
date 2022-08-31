16d9f937204422058db9995ddcfaf5f83f8d8545
tatotux <tatotux@pm.me>
2022-08-31 17:57:01 -0600

    Tarea 31 Agosto 2022

---
+++ Tato/Python/2022_08_31_tarea.py
@@ -0,0 +1,75 @@
+# Ejercicio 1:
+# Escribir un programa que un número entero y muestre en pantalla si es par o impar.
+# Así espero su respuesta en todos los ejercicios siguientes.
+numero = 46
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
+
+# Ejercicio 2:
+# Escribir un programa que un número entero y muestre en pantalla si es positivo, negativo o cero.
+
+# Ejercicio 3:
+# Escribir un programa que un número entero y muestre en pantalla si es divisible entre 5.
+
+# Ejercicio 4:
+# Escribir un programa que un número entero y muestre en pantalla si es divisible entre 3 y 5.
+
+# Ejercicio 5:
+# Escribir un programa que un número entero y muestre en pantalla si es divisible entre 3 o 5.
+
+# Ejercicio 6:
+# Escribir un programa que muestre en pantalla la cantidad de elementos en una lista.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 5
+
+# Ejercicio 7:
+# Escribir un programa que muestre en pantalla la suma de todos los elementos de una lista.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 15
+
+# Ejercicio 8:
+# Escribir un programa que muestre en pantalla la suma de todos los elementos de una lista que sean pares.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 6
+
+# Ejercicio 9:
+# Escribir un programa que muestre en pantalla la suma de todos los elementos de una lista que sean impares.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 9
+
+# Ejercicio 10:
+# Escribir un programa que muestre en pantalla la suma de todos los números de 2 listas.
+# Ejemplo: 
+# lista1 = [1, 2, 3, 4, 5]
+# lista2 = [6, 7, 8, 9, 10]
+# suma = 55 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
+
+# Ejercicio 11:
+# Escribir un programa que muestre en pantalla solo los index de los elementos de una lista que sean pares.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = [3, 5] (Por que los pares son el index 2 es par y el index 4 es par)
+
+# Ejercicio 12:
+# Escribir un programa que muestre en pantalla el último elemento de una lista.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 5
+
+# Ejercicio 13:
+# Escribir un programa que muestre en pantalla el primer elemento de una lista.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 1
+
+# Ejercicio 14:
+# Escribir un programa que muestre en pantalla promedio de los elementos de una lista.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 15/5 = 3
+
+# Ejercicio 15:
+# Escribir un programa que muestre en pantalla el número menor de una lista.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 1
+
+# Ejercicio 16:
+# Escribir un programa que muestre en pantalla el número mayor de una lista.
+# Ejemplo: lista = [1, 2, 3, 4, 5] = 5
+
+# Ejercicio 17:
+# Escribir un programa que muestre en pantalla la unión de dos listas.
+# Ejemplo: lista1 = [1, 2, 3, 4, 5] lista2 = [6, 7, 8, 9, 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Ejercicio 18:
+# Escribir un programa que muestre en pantalla el número mayor entre 2 listas.
+# Ejemplo: lista1 = [1, 2, 3, 4, 5] lista2 = [6, 7, 8, 9, 10] el número mayor es 10
